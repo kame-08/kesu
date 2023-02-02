eae1e64ac5391839d4541851e70a17bb723244c3
inotomo06 <21cm0106@jec.ac.jp>
2023-02-02 23:03:58 +0900

    Initial Commit

---
+++ kesu/kesuApp.swift
@@ -0,0 +1,17 @@
+//
+//  kesuApp.swift
+//  kesu
+//
+//  Created by Ryo on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct kesuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
